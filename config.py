f13c018de6a824f6d869ec8313124feb57c2d435
otayuji2017business-cloud <otayuji2017business@gmail.com>
2026-02-13 21:30:49 +0900

    修正config.py理由：os.environ["DB_PASSWORD"]未設定のため

--- config.py
+++ config.py
@@ -1,20 +1,13 @@
 import os
 
-class DevConfig:
-    DATABASE_URL = (
-        f"mysql+pymysql://appuser:openPass0301@"
-        f"@127.0.0.1:3306/appdb"
-    )
-
 class ProdConfig:
-    DATABASE_URL = (
-        "mysql+pymysql://appuser:"
-        + os.environ["DB_PASSWORD"]
-        + "@/appdb"
-        + "?unix_socket=/cloudsql/platinum-linker-487308-t8:asia-northeast1:flask-mysql"
-    )
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
 
-def get_config():
-    if os.environ.get("FLASK_ENV") == "development":
-        return DevConfig
-    return ProdConfig
+    if not DB_PASSWORD:
+        raise RuntimeError("DB_PASSWORD environment variable not set")
+
+    DATABASE_URL = (
+        f"mysql+pymysql://appuser:{DB_PASSWORD}"
+        f"@/appdb"
+        f"?unix_socket=/cloudsql/platinum-linker-487308-t8:asia-northeast1:flask-mysql"
+    )
